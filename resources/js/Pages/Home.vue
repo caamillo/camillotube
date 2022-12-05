59cb97162e3478a68ec371ae2c61dc8f184aa0db
Matteo Camillo Ludovico <camillo@DESKTOP-PNEPTL4.localdomain>
2022-12-05 13:20:20 +0100

    init

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="">
+        Ciao
+    </div>
+</template>
---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+// app.js
+
+import * as bootstrap from './bootstrap'
+import { createApp } from 'vue'
+
+import Home from './Pages/Home.vue'
+
+createApp(Home).mount("#app")
