515541b74def1b3873e8bde3d1a63b206e49f737
caamillo <diaskygotah@gmail.com>
2022-12-06 11:33:56 +0100

    skeletonLoading

---
+++ resources/js/Components/SkeletonVideo.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="rounded-lg cursor-pointer w-[380px] m-2 p-3 hover:bg-black flex flex-col">
+        <div class="rounded-md skeletonAnim h-[200px] w-full object-cover">
+        </div>
+        <div class="flex items-center space-x-3">
+            <div class="rounded-full w-[30px] aspect-square skeletonAnim"></div>
+            <div class="desc p-2 space-y-3 w-full">
+                <div class="h-3 rounded-md w-[80%] skeletonAnim"></div>
+                <div class="h-2.5 rounded-md w-[40%] skeletonAnim"></div>
+                <div class="h-2.5 rounded-md w-[80%] skeletonAnim"></div>
+            </div>
+        </div>
+    </div>
+</template>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,13 +1,13 @@
 <template>
     <DialogVideo :addVideo="addVideo" :show="isDialogVideoOpen" :close-dialog-video="closeDialogVideo" @close="closeDialogVideo" />
     <Disclosure as="nav" v-slot="{ open }">
-        <div class="mx-auto max-w-7xl px-2 px-8">
+        <div class="mx-auto max-w-7xl px-8">
             <div class="relative flex h-16 items-center justify-between">
                 <div class="font-bold text-xl space-x-1">
                     <span>Camillo</span>
                     <span class="bg-gray-500/50 rounded-md p-1 px-2 text-[#121212]">Tube</span>
                 </div>
-                <div class="flex flex-1 space-x-3 items-center justify-center items-stretch justify-start">
+                <div class="flex flex-1 space-x-3 justify-center items-stretch">
                     <input class="w-[50%] bg-black h-[2.5rem] p-2 focus:outline-none border-2 border-gray-500/50 rounded-md" placeholder="Search..."/>
                     <button class="bg-gray-500/50 px-5 rounded-md text-black">
                         <MagnifyingGlassIcon class="w-5 cursor-pointer"/>
@@ -25,7 +25,8 @@
                         <div>
                             <MenuButton class="flex rounded-full text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-[#121212]">
                                 <span class="sr-only">Open user menu</span>
-                                <object class="h-8 w-8 rounded-full object-cover hover:bg-black" :data="user.pfp" type="image/png"></object>
+                                <object v-if="videosready.value" class="h-8 w-8 rounded-full object-cover hover:bg-black" :data="user.pfp" type="image/png"></object>
+                                <div v-else class="h-8 w-8 rounded-full skeletonAnim" ></div>
                             </MenuButton>
                         </div>
                         <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
@@ -48,7 +49,8 @@
     </Disclosure>
     <div class="flex justify-center">
         <div class="grid 2xl:grid-cols-4 xl:grid-cols-3 md:grid-cols-2 grid-cols-1">
-            <Video v-for="video in videos" :key="video.id" :videoDetails="video" />
+            <Video v-if="videosready.value" v-for="video in videos" :key="video.id" :videoDetails="video" />
+            <SkeletonVideo v-else v-for="(n, index) in videotoload" />
         </div>
     </div>
 </template>
@@ -58,10 +60,30 @@ export default {
     data() {
         return {
             videos: [],
+            users: [],
+            previews: [],
+            usersavatars: [],
+            videotoload: 5,
+            videosready: {
+                value: false
+            },
             user: {
                 uid: 12412125,
                 name: 'caamillo',
                 pfp: ''
+            }
+        }
+    },
+    computed: {
+        isVideoDone() {
+            return [this.users, this.previews, this.usersavatars]
+        }
+    },
+    watch: {
+        isVideoDone: {
+            deep: true,
+            handler: function([users, previews, usersavatars]) {
+                this.unghostElements(users, previews, usersavatars)
             }
         }
     },
@@ -78,26 +100,52 @@ export default {
                 preview: URL.createObjectURL(await (await this.getRndImg()).blob()),
                 title: title
             })
+        },
+        unghostElements(users, previews, usersavatars) {
+            if (users.length < this.videotoload + 1) return
+            else if (usersavatars.length < this.videotoload + 1) return
+            else if (previews.length < this.videotoload) return
+            this.videosready.value = true
+            this.user.pfp = this.users[this.videotoload].avatar
+            for (let i = 0; i < this.videotoload; i++) {
+                this.videos.push({
+                    id: Math.floor(Math.random() * (9999999 - 1000000) + 1000000),
+                    title: `Titolo Video No.${ i }`,
+                    user: {
+                        uid: users[i].uid,
+                        name: users[i].username,
+                        pfp: users[i].avatar
+                    },
+                    uploaded: new Date(),
+                    views: Math.floor(Math.random() * 1000),
+                    duration: Math.floor(Math.random() * 1000),
+                    preview: previews[i]
+                })
+            }
         }
     },
-    async mounted() {
-        const numvideos = 5
-        const users = await (await this.getRndUsrs(numvideos + 1)).json()
-        this.user.pfp = users[numvideos].avatar
-        for (let i = 0; i < numvideos; i++) {
-            this.videos.push({
-                id: Math.floor(Math.random() * (9999999 - 1000000) + 1000000),
-                title: `Titolo Video No.${ i }`,
-                user: {
-                    uid: users[i].uid,
-                    name: users[i].username,
-                    pfp: users[i].avatar
-                },
-                uploaded: new Date(),
-                views: Math.floor(Math.random() * 1000),
-                duration: Math.floor(Math.random() * 1000),
-                preview: URL.createObjectURL(await (await this.getRndImg()).blob())
+    mounted() {
+        console.log('ciao')
+        this.getRndUsrs(this.videotoload + 1)
+            .then(res => res.json())
+            .then(data => {
+                this.users.push(...data)
+                for (let user of data) {
+                    fetch(user.avatar)
+                        .then(res => res.blob())
+                        .then(blobImg => {
+                            const blobImgURL = URL.createObjectURL(blobImg)
+                            this.usersavatars.push(blobImgURL)
+                        })
+                }
             })
+        for (let i = 0; i < this.videotoload; i++) {
+            this.getRndImg()
+                .then(res => res.blob())
+                .then(blobImg => {
+                    const blobImgURL = URL.createObjectURL(blobImg)
+                    this.previews.push(blobImgURL)
+                })
         }
     }
 }
@@ -105,10 +153,11 @@ export default {
 
 <script setup>
 import { Disclosure, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
-import { MagnifyingGlassIcon, BellIcon } from '@heroicons/vue/24/outline'
+import { MagnifyingGlassIcon } from '@heroicons/vue/24/outline'
 import { ref } from 'vue'
 import Video from '../Components/Video.vue'
 import DialogVideo from '../Components/DialogVideo.vue'
+import SkeletonVideo from '../Components/SkeletonVideo.vue'
 
 const isDialogVideoOpen = ref(false)
 
@@ -120,3 +169,18 @@ const closeDialogVideo = () => {
     isDialogVideoOpen.value = false
 }
 </script>
+
+<style>
+    .skeletonAnim {
+        background-color: #1d1d1d;
+        animation: skeletonAnim 1.5s alternate infinite ease-in-out;
+    }
+    @keyframes skeletonAnim {
+        from {
+            background-color: #1d1d1d;
+        }
+        to {
+            background-color: #343434;
+        }
+    }
+</style>
