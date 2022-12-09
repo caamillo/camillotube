27113f6f02bcb1987b2847ac5416782bb354b3d1
caamillo <diaskygotah@gmail.com>
2022-12-09 14:39:39 +0100

    migration

---
+++ database/migrations/2022_12_08_100756_create_videos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('desc');
+            $table->timestamp('uploaded');
+            $table->integer('views');
+            $table->integer('duration');
+            $table->integer('userid');
+            $table->binary('preview');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('videos');
+    }
+};
